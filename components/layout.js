1b5de3c450e60c153a631981544e546cd73619d3
Juan Saavedra <juansaav_@hotmail.com>
2024-01-30 08:41:31 -0300

    Modify text

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from './layout.module.css';
 import utilStyles from '../styles/utils.module.css';
 import Link from 'next/link';
 
-const name = 'Test Name';
+const name = 'Next.js blog example';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
--- pages/index.js
+++ pages/index.js
@@ -21,11 +21,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Next.js tutorial</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+        <p>Please find below the list of blogs generated using SSR</p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
