2f2d283ef0276f625c590fdb85ae5ccf4fc982eb
Juan Saavedra <juansaav_@hotmail.com>
2024-02-12 09:07:36 -0300

    Some adjustments.

--- pages/new-post.js
+++ pages/new-post.js
@@ -33,7 +33,18 @@ const NewPost = () => {
             role: "user",
             content: `You are an expert at writing blogs.
                       I want you to suggest 10 topics for this new blog with the following title: ${title}.
-                      The response must be a list of strings. Don't wrap each suggestion with quotes.`,
+                      The response must be a list of strings. Don't wrap each suggestion with quotes.
+                      ###
+                      Here are some examples for the title 'Animals':
+                      1. Top 10 Endangered Animals and How to Help
+                      2. The Role of Animals in Our Ecosystems
+                      3. The Most Intelligent Animals in the World
+                      4. 50 Unique Animals You've Never Heard Of
+                      ###`,
+            // Few shot learning: Few-shot learning or in-context learning is when we give a few examples in the prompts, and the LLM can generate corresponding output based on the example demonstrations.
+            // Delimiter: Delimiters like ###, <<< >>> specify the boundary between different sections of the text. In our example, we used ### to indicate examples and <<<>>> to indicate customer inquiry.
+            // Role playing: Providing LLM a role (e.g., "You are a bank customer service bot.") adds personal context to the model and often leads to better performance.
+            safe_mode: true,
           },
         ],
       });
